282d95b018689d0dec2fd8abe459ad2d2e91a5e6
lanceJiang <913728452@qq.com>
2022-11-27 21:25:17 +0800

    feat: xiaopubao-ui init

---
+++ README.md
@@ -0,0 +1,19 @@
+# xiaopubao-ui
+基于 vue-cli3 的 UI 组件库
+
+### Install
+```
+npm install xiaopubao-ui -S
+```
+
+### Usage
+```
+import Vue from "vue"
+import XPBUI from 'xiaopubao-ui'
+import 'xiaopubao-ui/style/index.less'
+Vue.use(XPBUI)
+```
+在项目中使用
+```
+<x-component></x-component>
+```
---
+++ vue.config.js
@@ -0,0 +1,46 @@
+const path = require('path')
+
+module.exports = {
+  devServer: {
+    port: 7777
+  },
+  css: {
+    sourceMap: true
+  },
+  pages: {
+    index: {
+      entry: 'examples/main.js',
+      template: 'public/index.html',
+      filename: 'index.html',
+      // template 中的 title 标签需要是 <title><%= htmlWebpackPlugin.options.title %></title>
+      title: 'xiaopubao-ui'
+    }
+  },
+  chainWebpack: config => {
+    // vue默认@指向src目录，这里要修正为examples，另外新增一个~指向packages
+    config.resolve.alias
+      .set('xiaopubao-ui', path.resolve(''))
+      .set('@', path.resolve('examples'))
+
+    // packages和examples目录需要加入编译
+    config.module
+      .rule('js')
+      .include.add(/packages/).end()
+      .include.add(/examples/).end()
+      .use('babel')
+      .loader('babel-loader')
+      .tap(options => {
+        // 修改它的选项...
+        return options
+      })
+  },
+  pluginOptions: {
+    'style-resources-loader': {
+      preProcessor: 'scss',
+      patterns: [
+        path.resolve(__dirname, 'examples/assets/css/variables.less'),
+        path.resolve(__dirname, 'style/variables.less')
+      ]
+    }
+  }
+}
